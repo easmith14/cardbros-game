1e3dc94f4296cfea58046181e5d946e2e1b4b632
Ben Smith <benjaminsmith5759@gmail.com>
2022-12-21 23:43:44 -0500

    Ben's commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ben is the best.
+          Evan is the best.
         </p>
         <a
           className="App-link"
